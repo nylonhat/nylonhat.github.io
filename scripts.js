4cb2236bd3b29070a5e42c63dfe8b5a4830e91b1
Your NameNylonhat <nylonhat@gmail.com>
2020-11-28 17:47:07 +1030

    update margin

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleDisplay(ElementId) {
+	var x = document.getElementById(ElementId);
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
