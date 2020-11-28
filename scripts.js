1f7a1810b04a6a845dd88c24e33851abf528ef70
Your NameNylonhat <nylonhat@gmail.com>
2020-11-28 20:33:58 +1030

    update margin

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ function toggleDisplay(ElementId) {
 	var x = document.getElementById(ElementId);
 	if (x.style.display === "none") {
 	  x.style.display = "block";
+	  x.style.height = "20em";
 	} else {
 	  x.style.display = "none";
+	  x.style.height = "0em";
 	}
   }
