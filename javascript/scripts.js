2cc8eb431799a18cf2fb737a97b8b9778b8b1af7
Your NameNylonhat <nylonhat@gmail.com>
2021-01-07 20:29:04 +1030

    table style

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -11,7 +11,7 @@ function toggleDisplay(ElementId) {
 
 
 function initExpandableLinks(){
-	var dropdown = document.getElementsByClassName("navlinks expandable-link");
+	var dropdown = document.getElementsByClassName("navlinks");
 	var i;
 	alert(dropdown.length);
 	for (i = 0; i < dropdown.length; i++) {
